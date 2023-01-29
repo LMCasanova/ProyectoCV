279b7daabf3e79988c7b8d441ce31c57e8f7b97c
LMCasanova <lautaromcasanova@gmail.com>
2023-01-29 16:25:49 -0600

    agregue hoja de js

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function cambiareltextodelbodycard() {
+    document.getElementById("desceric").innerHTML = "Gerente de  Proyecto en Acme Inc. En esta posición, Eric lideró un equipo de desarrollo de software para la implementación de un sistema de gestión de proyectos en línea. Logró cumplir con el plazo y el presupuesto establecidos, y recibió elogios por su capacidad para motivar y liderar efectivamente a su equipo.";
+}
+
+document.getElementById("btnexp").addEventListener("click", cambiareltextodelbodycard);
+
+function cambiareltextodelbodycard1() {
+    document.getElementById("desceric").innerHTML = "Manejo de Lenguajes de Programacion: Python, Ruby";
+}
+
+document.getElementById("btnhab").addEventListener("click", cambiareltextodelbodycard1);
